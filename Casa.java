a5974263d4bf50b33ac1ea2b425568e1a6130132
mariojr04 <162380419+mariojr04@users.noreply.github.com>
2025-05-21 21:06:46 -0300

    Update Casa.java

--- Casa.java
+++ Casa.java
@@ -14,12 +14,12 @@ public class Casa extends Imovel {
     public Boolean getQuintal(){
         return this.quintal;
     }
-
-    public Double ValorImovel(int qntQuartos, Boolean quintal){
-        if (qntQuartos > 4 || quintal){
-            return this.getPrecoCompra() * 0.3;
+    @Override
+    public Double PrecoVenda(){
+        if (getQntQuartos > 4 || quintal){
+            return getPrecoCompra() * 0.3;
         } else {
-            return this.getPrecoCompra() * 0.15;
+            return getPrecoCompra() * 0.15;
         }
     }
 }
